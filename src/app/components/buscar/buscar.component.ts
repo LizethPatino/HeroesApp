1f446b5248310266c1755e0aeb868c8f9eb54b58
LizetPatino <lizeth0330@outlook.com>
2019-02-20 20:09:41 -0500

    adición componente hijo

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from './../../services/heroes.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-buscar',
@@ -10,15 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class BuscarComponent implements OnInit {
   heroes : Heroe[] = [];
   subtitulo: any;
-  constructor(private _heroesService: HeroesService,  private activatedRoute:ActivatedRoute) {
+  termino: string;
+  constructor(private _heroesService: HeroesService,  private activatedRoute:ActivatedRoute,  private router:Router) {
+
+ }
+ verHeroe(index:number){
+  this.router.navigate(['/heroe', index ]);
+
+}
+  ngOnInit() {
     this.activatedRoute.params.subscribe( params => {
       this.heroes= this._heroesService.buscarHeroes( params['nombre'] );
       this.subtitulo = this.heroes.length.toString();
+      this.termino=params['nombre'];
 });
   }
-
-  ngOnInit() {
-  }
-
 
 }
